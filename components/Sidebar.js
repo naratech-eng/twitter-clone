5d1e147f5352d4b858dba0569f0fe0b9c784905e
Sanjeewa Narayana <narayans@uoguelph.ca>
2022-10-12 15:43:48 -0400

    initial setup

---
+++ components/Sidebar.js
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import React from 'react'
+
+import logo from '../images/logo.png'
+
+const Sidebar = () => {
+  return (
+    <div>Sidebar
+    {/* logo */}
+
+    <Image src={logo} width={64} height={64}/>
+
+    {/* menu */}
+
+    {/* Button */}
+
+    {/* Mini-profile */}
+
+
+    
+    </div>
+  )
+}
+
+export default Sidebar
